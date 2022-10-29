1fd1150318a0a91f0bb56ec58552bab39772a933
Chen Mingliang <illuspas@msn.com>
2022-10-29 18:38:55 +0800

    f

--- test/relay_live_stream.js
+++ test/relay_live_stream.js
@@ -2,5 +2,5 @@ const FFmpeg = require('..');
 
 new FFmpeg()
   .Input('rtmp://192.168.0.2/live/bbb')
-  .Output('rtmp://live.nodemedia.cn/live/sss', FFmpeg.Copy(), FFmpeg.Format('flv'))
+  .Output('rtmp://192.168.0.3/live/bbb', FFmpeg.Copy(), FFmpeg.Format('flv'))
   .Run('/usr/local/bin/ffmpeg');
