43225e31e33fe588fcfc0912d9e9c4e3961a9ae8
Chen Mingliang <illuspas@msn.com>
2022-10-30 20:22:02 +0800

    test 'save live stream' using copy stream

--- test/save_live_stream.js
+++ test/save_live_stream.js
@@ -2,5 +2,5 @@ const FFmpeg = require('..');
 
 new FFmpeg()
   .Input('rtmp://192.168.0.2/live/bbb')
-  .Output('bbb.mp4')
+  .Output('bbb.mp4', FFmpeg.Copy())
   .Run('/usr/local/bin/ffmpeg', FFmpeg.OverWriteOutput());
